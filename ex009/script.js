81c80599f660a7847d5ae651f84409f210e0c5c9
pedrohenriquesilvamarques17-coder <francinalvabrodrigues@unirg.edu.br>
2026-03-13 20:05:02 -0300

    Merge branch 'main' of https://github.com/francinalvarodrigues/dw-frontend

---
+++ ex009/script.js
@@ -0,0 +1,25 @@
+var nota1 = 5;
+var nota2 = 7;
+
+var media = (nota1 + nota2) / 2;
+
+console.log('A nota do aluno é:$(media)');
+
+/* As condições são Se o aluno tem  nota maior que 7 ele está aprovado, caso contrário ele está reprovado */
+
+if (média>= 7){
+console.log('O aluno está aprovada!')
+} else{
+
+console.log('O aluno está reprovado!')
+}
+
+
+if(media>= 7){
+console.log('O aluno está aprovado!')
+
+} else if(media <6){
+console.log('O aluno está reprovado!')
+} else{
+    console.log('O aluno está de recuperação')
+}
